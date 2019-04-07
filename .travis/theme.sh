6773adea7904589237196f7fcccc6dd0c0d74f47
wuzk <jobjoy@qq.com>
2019-04-07 13:10:11 +0800

    2019-04-07 update

--- .travis/theme.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [[ -d "themes/next" ]]; then
-  cd themes/next;
-  git pull;
-  cd ../../;
-else
-  git clone https://github.com/theme-next/hexo-theme-next themes/next;
-fi
-cp .travis/theme_config.yml themes/next/_config.yml;
